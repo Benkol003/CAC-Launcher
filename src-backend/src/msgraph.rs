d3ea316c338ed1067a2cbd3c1a75dbfa41687016
Ben Kollar <benjkollar03@gmail.com>
2025-05-13 00:18:20 +0100

    using serde deserialisation for msgraph json responses

--- src-backend/src/msgraph.rs
+++ src-backend/src/msgraph.rs
@@ -2,18 +2,23 @@
 #![allow(unreachable_code)]
 #![allow(unused_imports)]
 #![allow(unused_import_braces)]
-
 #![allow(warnings)]
 
-use std::collections::HashMap;
-use anyhow::{anyhow,Error};
+use anyhow::{ anyhow, Error };
+use serde_json::map::Entry;
+use serde::{ Deserialize };
+use std::{ collections::HashMap, io::Read };
 use stopwatch::Stopwatch;
 use urlencoding;
 
-use serde::Deserialize;
-use base64::{self, prelude::{BASE64_STANDARD_NO_PAD, BASE64_URL_SAFE_NO_PAD},Engine};
+use base64::{ self, prelude::{ BASE64_STANDARD_NO_PAD, BASE64_URL_SAFE_NO_PAD }, Engine };
 
-use reqwest::{blocking::{get,Client}, header::{self, HeaderMap, CONTENT_TYPE, HOST}, Version};
+use reqwest::{
+    blocking::{ get, Client },
+    header::{ self, HeaderMap, CONTENT_TYPE, HOST },
+    Url,
+    Version,
+};
 
 use crate::secrets;
 
@@ -21,87 +26,142 @@ use crate::secrets;
 
 const TENANT_ID: &str = "4fd01353-8fd7-4a18-a3a1-7cd70f528afa";
 const APP_CLIENT_ID: &str = "9ecaa0e8-9caf-4f49-94e8-8430bbf57486";
+const MSAPI_URL: &str = "https://graph.microsoft.com/v1.0/";
 //const MSGPRAPH_KEY - place in secrets.rs
 
-//see https://learn.microsoft.com/en-us/graph/api/shares-get?view=graph-rest-1.0&tabs=http
-
-#[derive(Deserialize)]
-struct TokenResponse{
-    token_type : String,
-    expires_in: usize,
-    ext_expires_in: usize,
-    access_token: String
+///[msgraph reference](https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token)
+#[derive(Deserialize)] //TODO remove?
+pub struct TokenResponse {
+    pub token_type: String,
+    pub expires_in: usize,
+    pub ext_expires_in: usize,
+    pub access_token: String,
 }
 
-#[derive(Deserialize,Debug)]
-struct sharedDriveItem {
-    id: String,
-    name: String,
-    owner: String,
+
+/// [msgraph reference](https://learn.microsoft.com/en-us/graph/api/resources/driveitem?view=graph-rest-1.0)
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SharedDriveItem {
+    /// the encoded sharing url for making new requests. This is not part of the msgraph DriveItem json response.
+    #[serde(skip)]
+    pub share_id: String,
+    pub name: String,
+    pub size: usize,
+    #[serde(flatten)]
+    pub item: FsEntryType,
 }
 
-pub fn get_shared_drive_item(client: &Client, token: &str, url: &str) -> Result<serde_json::Value,Error> {
-let client = reqwest::blocking::Client::new();
-//let mut params = HashMap::new();
+// #[derive(Deserialize, Debug)]
+// #[serde(rename_all = "camelCase")]
+// pub struct FileItem {
+//     pub quick_xor_hash: String,
+// }
 
-let encodedShareUrl = format!("u!{}",BASE64_URL_SAFE_NO_PAD.encode(url.as_bytes()));
-println!("encoded url: {encodedShareUrl}");
+// #[derive(Deserialize, Debug)]
+// pub struct FolderItem {
+//     pub child_count: usize,
+// }
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Hashes {
+    pub quick_xor_hash: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub enum FsEntryType {
+    File {
+        hashes: Hashes,
+    },
+    #[serde(rename_all = "camelCase")]
+    Folder {child_count: usize},
+}
+
+/// [msgraph reference](https://learn.microsoft.com/en-us/graph/api/shares-get?view=graph-rest-1.0&tabs=http)
+pub fn get_encoded_sharing_url(client: &Client, url: Url) -> Result<String, Error> {
+    let response = client.get(url).send()?;
+    let final_url = response.url();
+    return Ok(format!("u!{}", BASE64_URL_SAFE_NO_PAD.encode(final_url.as_str())));
+}
+
+/// [msgraph reference](https://learn.microsoft.com/en-us/graph/api/shares-get?view=graph-rest-1.0&tabs=http)
+pub fn get_shared_drive_item(
+    client: &Client,
+    token: &str,
+    url: &str
+) -> Result<SharedDriveItem, Error> {
+    let client = reqwest::blocking::Client::new();
+    //let mut params = HashMap::new();
+
+    let share_id = format!("u!{}", BASE64_URL_SAFE_NO_PAD.encode(url.as_bytes()));
+    println!("encoded url: {share_id}");
     let mut headers = HeaderMap::new();
-    headers.append(header::AUTHORIZATION, format!("Bearer {}",token).parse()?);
-    headers.append(header::CONTENT_TYPE,"application/json".parse()?);
-    headers.append("prefer","redeemSharingLink".parse()?);
-
-    let msapi_url = "https://graph.microsoft.com/v1.0/";
-    let mut clock = Stopwatch::start_new();
-    let response = client.get(format!("{}shares/{}/driveItem",msapi_url,encodedShareUrl)).headers(headers).send()?;
-    clock.stop();
-    println!("getSharedDriveItem response time: {}ms",clock.elapsed_ms());
-    if(response.status().as_u16() != 200) {
-        return Err(anyhow!("http error - code {}, text: {}",response.status().as_u16(),response.text()?));
+    headers.append(header::AUTHORIZATION, format!("Bearer {}", token).parse()?);
+    headers.append(header::CONTENT_TYPE, "application/json".parse()?);
+    headers.append("prefer", "redeemSharingLink".parse()?);
+    let mut response = client
+        .get(format!("{}shares/{}/driveItem", MSAPI_URL, share_id))
+        .headers(headers.clone())
+        .send()?;
+    if response.status().as_u16() != 200 {
+        return Err(
+            anyhow!("http error - code {}, text: {}", response.status().as_u16(), response.text()?)
+        );
     }
-    //println!("sharedDriveItem:\n{sharedDriveItem}");
+
+    let body = client
+        .get(format!("{}shares/{}/driveItem", MSAPI_URL, share_id))
+        .headers(headers)
+        .send()?
+        .text()?;
+    println!("response: \n{}", body);
 
     return Ok(response.json()?);
 }
 
-//not valid for app token / us TODO REMOVE
-pub fn login_info(client: &Client, token: &str) -> Result<(),Error>{
+/// [msgraph reference](https://learn.microsoft.com/en-us/graph/api/driveitem-get-content?view=graph-rest-1.0&tabs=http)
+pub fn download_item(client: &Client, token: &str, url: &str) -> Result<(), Error> {
+    let encodedShareUrl = format!("u!{}", BASE64_URL_SAFE_NO_PAD.encode(url.as_bytes()));
     let mut headers = HeaderMap::new();
-    headers.insert(header::AUTHORIZATION, format!("Bearer {}",token).parse()?);
-    headers.insert(header::HOST,"graph.microsoft.com".parse()?);
+    headers.append(header::AUTHORIZATION, format!("Bearer {}", token).parse()?);
 
-    let response = client.get("https://graph.microsoft.com/v1.0/me/").version(Version::HTTP_11).headers(headers).send()?;
+    let response = client
+        .get(format!("{}shares/{}/driveItem/content", MSAPI_URL, encodedShareUrl))
+        .headers(headers)
+        .send()?;
 
-    if(!response.status().is_success()){
-        return Err(anyhow!("failed to get login info - http error code {}, text: {}",response.status().as_u16(),response.text()?));
-    }
+    println!("response code: {}", response.status());
+    println!("response : {}", response.bytes()?.len());
 
-    let info = response.text()?;
-    println!("login info:\n{}",info);
-
-    return Ok(());   
+    Ok(())
 }
 
-//TODO: switch to using a certificate 
-pub fn login(client: &Client) -> Result<String,Error> {
-//get an access token
-let mut params = HashMap::new();
-params.insert("client_id",APP_CLIENT_ID);
-params.insert("scope","https://graph.microsoft.com/.default");
-params.insert("client_secret",secrets::MSGRAPH_KEY);
-params.insert("grant_type","client_credentials");
+///[msgraph reference](https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token)
+///TODO: switch to using a certificate
+pub fn login(client: &Client) -> Result<String, Error> {
+    //get an access token
+    let mut params = HashMap::new();
+    params.insert("client_id", APP_CLIENT_ID);
+    params.insert("scope", "https://graph.microsoft.com/.default");
+    params.insert("client_secret", secrets::MSGRAPH_KEY);
+    params.insert("grant_type", "client_credentials");
 
-let response = client.post(format!("https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"))
-.header(HOST, "login.microsoftonline.com")
-.header(CONTENT_TYPE,"application/x-www-form-urlencoded")
-.form(&params)
-.send()?;
+    let response = client
+        .post(format!("https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"))
+        .header(HOST, "login.microsoftonline.com")
+        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .form(&params)
+        .send()?;
 
-if response.status().is_success() {
-    //println!("access token: {}",response.text().as_ref()?);
-    let json: TokenResponse = response.json()?;
-    return Ok(json.access_token);
-}else{
-    return Err(anyhow!("failed to get access token. status code: {}",response.status().as_str()));
-};      
+    if response.status().is_success() {
+        //println!("access token: {}",response.text().as_ref()?);
+        let json: TokenResponse = response.json()?;
+        return Ok(json.access_token);
+    } else {
+        return Err(
+            anyhow!("failed to get access token. status code: {}", response.status().as_str())
+        );
+    }
 }
--- src-backend/tests/tests.rs
+++ src-backend/tests/tests.rs
@@ -4,43 +4,79 @@
 #![allow(unused_import_braces)]
 
 use src_backend::*;
-use anyhow::{anyhow,Error,Context};
+use anyhow::{ anyhow, Error, Context };
 
 use src_backend::*;
 #[cfg(test)]
 mod tests {
+    use src_backend::msgraph::FsEntryType;
+
     use super::*;
 
     #[test]
-    fn msgraph_folder_link() -> Result<(),Error> {
-        println!("test enrty point");
+    fn msgraph_folder_link() -> Result<(), Error> {
+        println!("test entry point");
         //SPE folder link to multi parts
         let url = "https://tinyurl.com/2p9k9dsn";
-        let url = "https://0jz1q-my.sharepoint.com/:f:/g/personal/brenner650_0jz1q_onmicrosoft_com/EvzzpAaqTodBss4ZD441otIBFQzXx3xQs7XQhz0_YpiZzg?e=9rz54p";
+        let url =
+            "https://0jz1q-my.sharepoint.com/:f:/g/personal/brenner650_0jz1q_onmicrosoft_com/EvzzpAaqTodBss4ZD441otIBFQzXx3xQs7XQhz0_YpiZzg?e=9rz54p";
 
         let client_ctx = build_client_ctx()?;
         let token = msgraph::login(&client_ctx.client)?;
-        let item = msgraph::get_shared_drive_item(&client_ctx.client,&token,url)?;
+        let item = msgraph::get_shared_drive_item(&client_ctx.client, &token, url)?;
 
-        assert!(item.as_object().context("serde_json::Value.as_object failed")?.contains_key("folder"));
+        match item.item {
+            FsEntryType::File { hashes: _ } => {
+                panic!("shared drive item is file not folder");
+            }
+            FsEntryType::Folder { child_count: _ } => {}
+        }
 
-        println!("item:\n{}",serde_json::to_string_pretty(&item)?);
+        println!("item:\n{:?}", item);
         Ok(())
     }
 
     #[test]
-    fn msgraph_direct_link() -> Result<(),Error> {
-        //CBA_A3 direct download link
-        let url = "https://0jz1q-my.sharepoint.com/:u:/g/personal/brenner650_0jz1q_onmicrosoft_com/EeNzJRi0EWtAjUoEoakvWZgB4XmveMVxd6mH06fSPh6TBw?e=nZfx8s";
-
-
+    fn msgraph_download() -> Result<(), Error> {
         let client_ctx = build_client_ctx()?;
         let token = msgraph::login(&client_ctx.client)?;
-        let item = msgraph::get_shared_drive_item(&client_ctx.client,&token,url)?;
-        println!("item:\n{}",serde_json::to_string_pretty(&item)?);
+        let url =
+            "https://0jz1q-my.sharepoint.com/:u:/g/personal/brenner650_0jz1q_onmicrosoft_com/EeNzJRi0EWtAjUoEoakvWZgB4XmveMVxd6mH06fSPh6TBw?e=nZfx8s";
+        msgraph::download_item(&client_ctx.client, &token, url)?;
+        Ok(())
+    }
 
-        assert!(item.as_object().context("serde_json::Value.as_object failed")?.contains_key("file"));
+    #[test]
+    fn url_redirect() -> Result<(), Error> {
+        let url = "https://tinyurl.com/uvs5dkdj";
+        let client_ctx = build_client_ctx()?;
+        let response = client_ctx.client.get(url).send()?;
 
+        let new_url = response.url();
+        println!("final url: {}", new_url);
+
+        let token = msgraph::login(&client_ctx.client)?;
+        let item = msgraph::get_shared_drive_item(&client_ctx.client, &token, new_url.as_str())?;
+        println!("item:\n{:?}", item);
+
+        Ok(())
+    }
+
+    #[test]
+    fn msgraph_direct_link() -> Result<(), Error> {
+        //CBA_A3 direct download link
+        let url =
+            "https://0jz1q-my.sharepoint.com/:u:/g/personal/brenner650_0jz1q_onmicrosoft_com/EeNzJRi0EWtAjUoEoakvWZgB4XmveMVxd6mH06fSPh6TBw?e=nZfx8s";
+        let client_ctx = build_client_ctx()?;
+        let token = msgraph::login(&client_ctx.client)?;
+        let item = msgraph::get_shared_drive_item(&client_ctx.client, &token, url)?;
+        println!("item:\n{:?}", item);
+        match item.item {
+            FsEntryType::File { hashes: _ } => {}
+            FsEntryType::Folder { child_count: _ } => {
+                panic!("shared drive item is folder not file");
+            }
+        }
         Ok(())
     }
 }
